ee9817aa8a1afc736290f96eaf01cd8cbfe5c8ad
Glen <sj.yeom@fitpet.co.kr>
2022-02-14 10:50:33 +0900

    Initial commit

---
+++ src/test/java/com/example/springcloudconfigclienttest/SpringCloudConfigClientTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springcloudconfigclienttest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudConfigClientTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
